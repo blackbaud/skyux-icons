484a2f7de653cc0a5e87110d635b3976e5e2cb3e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2023-03-13 13:23:05 -0400

    add comment

--- module/get-icon-manifest.ts
+++ module/get-icon-manifest.ts
@@ -1,6 +1,9 @@
 import { ICON_MANIFEST } from './__manifest';
 import type { SkyThemeIconManifest } from './icon-manifest';
 
+/**
+ * Returns information about the SKY UX icon font.
+ */
 export function getIconManifest(): SkyThemeIconManifest {
   return ICON_MANIFEST;
 }
